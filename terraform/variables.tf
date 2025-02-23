2a7cfbe1568c4aca2f1d0ef781ba97a079c72d9c
bernie-cm <bernie.camejo@icloud.com>
2025-02-23 21:29:48 +1000

    added variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-southeast-2"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0b0a3a2350a9877be"
+}
